61db4e4e35c8c05790e2cff95792eb1ce9451c0f
audreyyun <96audreyyun@gmail.com>
2021-02-24 13:44:15 -0500

    Add complex Polypath

--- frontend/components/RouteMap/route_map.jsx
+++ frontend/components/RouteMap/route_map.jsx
@@ -12,18 +12,53 @@ import Navbar from '../Navbar'
 
 
 class RouteMap extends React.Component {
+    constructor(props) { 
+        super(props);
+        this.addLatLng = this.addLatLng.bind(this);
+    }
     componentDidMount() {
         // set the map to show SF
         const mapOptions = {
-            center: { lat: 37.7758, lng: -122.435 }, // this is SF
-            zoom: 13
+            center: { lat: 34.0745, lng: -118.3294 }, // this is SF
+            zoom: 7
         };
 
         // wrap this.mapNode in a Google Map
         this.map = new google.maps.Map(this.mapNode, mapOptions);
+
+        this.poly = new google.maps.Polyline({
+            strokeColor: "#000000",
+            strokeOpacity: 1.0,
+            strokeWeight: 3,
+        });
+        this.poly.setMap(this.map);
+        // Add a listener for the click event
+        this.map.addListener("click", this.addLatLng);
+    }
+
+    addLatLng(event) {
+        const path = this.poly.getPath();
+        // Because path is an MVCArray, we can simply append a new coordinate
+        // and it will automatically appear.
+        path.push(event.latLng);
+        // Add a new marker at the new plotted point on the polyline.
+        new google.maps.Marker({
+            position: event.latLng,
+            title: "#" + path.getLength(),
+            map: this.map,
+        });
     }
 
     render() {
+
+        const navbarProps =
+        {
+            loginBtnClass: "nav-btn-secondary",
+            loginBtnLabel: "Log Out",
+            loginBtnPath: "/logout",
+            isAuthenticated: true,
+        }
+
         return (
                 // ...
                 // this ref gives us access to the map dom node
